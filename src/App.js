8a37082a46ca93f3bb09284cb28684cf2cf8eb20
Gigher <gastelo1301@gmail.com>
2024-06-18 10:29:15 +0300

    add boolean buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { Provider, atom, useAtomValue } from 'jotai';
-import Counter from './components/Counter';
+import { Counter, SetFalseButton, SetTrueButton } from './components/Counter';
 import { CounterDisplay } from './components/CounterDisplay';
 
 
 const counterAtom = atom(0);
+const switchAtom = atom(false);
 
 function App() {
 
   const state = useAtomValue(counterAtom);
+  const state2 = useAtomValue(switchAtom);
   
   return (
       <div className="App">
         <Counter />
+        Counter: {state}
+        ---------------
+        State: <b>{state2.toString()}</b>
+        <SetTrueButton />
+        <SetFalseButton />
       </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -15,4 +15,32 @@ const Counter = () => {
   )
 }
 
-export default Counter
+
+// --------------------------------------------------------------------------
+
+const switchAtom = atom(false)
+
+const SetTrueButton = () => {
+  const setCount = useSetAtom(switchAtom)
+  const setTrue = () => setCount(true)
+
+  return (
+    <div>
+      <button onClick={setTrue}>Set True</button>
+    </div>
+  )
+}
+
+
+const SetFalseButton = () => {
+  const setCount = useSetAtom(switchAtom)
+  const setFalse = () => setCount(false)
+
+  return (
+    <div>
+      <button onClick={setFalse}>Set False</button>
+    </div>
+  )
+}
+
+export { Counter, SetTrueButton, SetFalseButton };
